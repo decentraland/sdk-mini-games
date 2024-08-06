4a3d49ec148a5bb1258f0da06bf740e08c92ef85
surz <36134492+surz90@users.noreply.github.com>
2024-08-07 00:31:05 +0700

    add game config (#4)
    * add game config
    
    * fix lint

---
+++ src/gameConfig/index.ts
@@ -0,0 +1,25 @@
+import { getSDK } from '../sdk'
+
+export enum ENV {
+  DEV = 'dev',
+  PRD = 'prd'
+}
+
+const GAME_SERVER_CONFIG: Record<string, string> = {
+  dev: 'https://exploration-games.decentraland.zone', //for local testing if you need different value
+  prd: 'https://exploration-games.decentraland.org' //PROD/live use this for launch
+}
+
+export let GAME_ID: string
+export let GAME_SERVER: string
+
+export function init() {
+  const { config } = getSDK()
+
+  GAME_ID = config.gameId
+  let _env = config.environment
+
+  if (_env !== ENV.PRD) _env = ENV.DEV
+
+  GAME_SERVER = GAME_SERVER_CONFIG[_env]
+}
