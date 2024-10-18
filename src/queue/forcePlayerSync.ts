7cbba989f66eb3600c1e110ea3ce90ec6b61f987
surz <36134492+surz90@users.noreply.github.com>
2024-10-18 21:17:50 +0700

    lint fix

--- src/queue/forcePlayerSync.ts
+++ src/queue/forcePlayerSync.ts
@@ -1,34 +1,34 @@
-import { getSDK } from "../sdk"
+import { getSDK } from '../sdk'
 
 /**
  * Cache the client userId
  */
 let userId: string | undefined
 function getUserId() {
-    if (userId) return userId
+  if (userId) return userId
 
-    const { players } = getSDK()
-    return (userId = players.getPlayer()?.userId)
+  const { players } = getSDK()
+  return (userId = players.getPlayer()?.userId)
 }
 function forceSelfPlayerSync() {
-    const {
-        engine,
-        syncEntity,
-        components: { Player }
-      } = getSDK()
+  const {
+    engine,
+    syncEntity,
+    components: { Player }
+  } = getSDK()
 
-    for (const [entity, player] of engine.getEntitiesWith(Player)) {
-        if (player.address === getUserId()) {
-          Player.getMutable(entity)
-        }
-      }
+  for (const [entity, player] of engine.getEntitiesWith(Player)) {
+    if (player.address === getUserId()) {
+      Player.getMutable(entity)
+    }
+  }
 }
 
 let count = 0
-export function ForceSelfPlayerSyncSystem(dt: number){
-    count += dt
-    if(count < 1) return
-    
-    count = 0
-    forceSelfPlayerSync()
+export function ForceSelfPlayerSyncSystem(dt: number) {
+  count += dt
+  if (count < 1) return
+
+  count = 0
+  forceSelfPlayerSync()
 }
